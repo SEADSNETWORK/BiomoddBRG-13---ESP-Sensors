93422ca25ffea2964e0b4ed642577025f2c2c470
U mom <you@example.com>
2021-06-16 14:43:55 +0200

    rgb

---
+++ Sensors/rgb/src/sensor.cpp
@@ -0,0 +1,16 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "sensor.h";
+
+void sensorSetup() {
+  //write setup code here
+}
+
+void sensorCode() {
+  //write loop code here
+}
