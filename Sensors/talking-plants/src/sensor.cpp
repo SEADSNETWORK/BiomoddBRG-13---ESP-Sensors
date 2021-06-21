49a40ed549cfc041fb2651c5afd732b460abe198
Pim Tournaye <pim.tournaye@student.ehb.be>
2021-06-21 14:57:42 +0200

    added talking plants to sensors - WIP

---
+++ Sensors/talking-plants/src/sensor.cpp
@@ -0,0 +1,60 @@
+#include "sensor.h"
+#include "json.h"
+#include "json.cpp"
+
+// defines here
+const int calatheaPin = A1;
+int motorPin[] = {5, 6, 10, 11};
+int potValue = 0;
+int motorValue = 0;
+
+const int loPlusPin = 3;
+const int loMinPin = 2;
+
+void sensorSetup() {
+  pinMode(loPlusPin, INPUT); // setup for lead off detection LO +
+  pinMode(loMinPin, INPUT); // setup for lead off detection LO -
+
+  for(int i = 0; i < 4; i++){
+    pinMode(motorPin[i], OUTPUT);
+  }
+  
+  Serial.begin(9600);
+}
+
+StaticJsonDocument<200> sensorCode() {
+  
+  if((digitalRead(loPlusPin) == 1) || (digitalRead(loMinPin) == 1)){
+    
+    potValue = 0;
+  }else{
+    //send value of analog input 0:
+    potValue = analogRead(calatheaPin);   
+  }
+  
+  Serial.write(potValue);
+  delay(1);
+  
+  motorValue = map(potValue, 0, 1023, 0, 255);
+
+  for(int i = 0; i < 4; i++){
+    digitalWrite(motorPin[i], motorValue);
+  }
+
+  //return jsonData;
+
+  RawData rawData[] = {
+    {.dataType = "sensor_value_1", .value = 0},
+    {.dataType = "sensor_value_2", .value = 0}
+  };
+
+  const size_t n = sizeof(rawData) / sizeof(rawData[0]);
+
+  std::array<RawData, n> convertedRawData;
+  std::copy(std::begin(rawData), std::end(rawData), convertedRawData.begin());
+
+  StaticJsonDocument<200> jsonData = convertToJson(convertedRawData);
+
+  return jsonData;
+
+}
