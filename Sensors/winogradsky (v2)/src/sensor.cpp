f93f187c3553957b06c1f522f08c42c0d133e965
Pim Tournaye <pim.tournaye@student.ehb.be>
2021-06-24 15:03:33 +0200

    winogradsky update

--- Sensors/winogradsky (v2)/src/sensor.cpp
+++ Sensors/winogradsky (v2)/src/sensor.cpp
@@ -11,7 +11,8 @@ unsigned long lastTime = 0;
 unsigned long timerDelay = 60000; // 60 seconds-> 60 datapoints/hr -> 1440 datapoint/24hrs.
 
 void sensorSetup() {
-  // sensor setup code here
+  Serial.println("Good Day :)");
+  Serial.begin(9600);
   // ADS1115 Setup
   Serial.println("Single-ended readings from AIN0 with >3.0V comparator");
   Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
@@ -34,30 +35,45 @@ void sensorSetup() {
   ads.begin();
 
   // Setup 3V comparator on channel 0
-  ads.startComparator_SingleEnded(0, 1000);
+  //int col1 = ads.startComparator_SingleEnded(0, 1000);
+  //int col2 = ads.startComparator_SingleEnded(1, 1000);
+  //int col3 = ads.startComparator_SingleEnded(2, 1000);
+  //int col4 - ads.startComparator_SingleEnded(3, 1000);
 }
 
 StaticJsonDocument<200> sensorCode() {
   
-  // Modified for testing from https://github.com/SEADSNETWORK/BiomoddBRG13-ESP32-ADS115-IoT/blob/main/ESP32_Biomodd_TSPEAK_05.ino
-
   //Get value from ADS1115
-    int16_t adc0;
+  int16_t adc0, adc1, adc2, adc3;
 
-    adc0 = ads.getLastConversionResults();
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
+  Serial.print("AIN0: "); Serial.println(adc0 * 0.125);
+  Serial.print("AIN1: "); Serial.println(adc1 * 0.125);
+  Serial.print("AIN2: "); Serial.println(adc2 * 0.125);
+  Serial.print("AIN3: "); Serial.println(adc3 * 0.125);
+  Serial.println(" ");
+
+    //adc0 = ads.getLastConversionResults();
+
+ 
 
     //Serial Print
-    Serial.println("Hello, this is my pulse:");
-    Serial.println(adc0 * 0.125); // single value (ico. Processing)
-    Serial.println("- - - - - - - - ");
+    //Serial.print("AIN0: "); Serial.println(adc0 * 0.125); // 1x gain example
+    //Serial.println("----------------");
+    //Serial.println("Hello, this is my pulse:");
+    //Serial.println(adc0 * 0.125); // single value (ico. Processing)
+    //Serial.println("- - - - - - - - ");
     delay(3000);
     //lastTime = millis();
 
-
-  //return jsonData;
-
   RawData rawData[] = {
-    {.dataType = "ADC Pulse", .value = adc0 * 0.125}
+    {.dataType = "Wino 1", .value = adc0 * 0.125},
+    {.dataType = "Wino 2", .value = adc1 * 0.125},
+    {.dataType = "Wino 3", .value = adc2 * 0.125},
+    {.dataType = "Wino 4", .value = adc3 * 0.125}
   };
 
   const size_t n = sizeof(rawData) / sizeof(rawData[0]);
